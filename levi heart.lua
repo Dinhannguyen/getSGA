31888df01e30e6e867fa08baf33e7221891f5633
Dinhannguyen <nguyenquocduynhanaov@gmail.com>
2026-02-26 15:03:07 +0700

    Update levi heart.lua

--- levi heart.lua
+++ levi heart.lua
@@ -1,31 +1,40 @@
 local username = game.Players.LocalPlayer.Name
 
 if username == "cutevidai" or username == "baohan_200" then
-    getgenv().Config = {
-        ["Shoot Heart When Ice Spike Breaks"] = true,
-        ["Drive Boat To Tiki"] = true,
-        ["No Frog"] = true,
-        ["Random Devil Fruit"] = false,
-        ["Use skill fast dont hold"] = true,
-        ["Select Sea Events"] = {},
-        ["Select Rarity Scroll "] = { [1] = "Mythical" },
-        ["Webhook Shoot Heart Leviathan"] = false,
-        ["Auto Farm Material Sanguine Art"] = false,
-        ["Webhook Unlock Draco v4"] = false,
-        ["Auto light the torch"] = false,
-        ["Webhook Destroy IDK"] = false,
-        ["Use Click M1 Fruit"] = true,
-        ["Ping Discord"] = false,
-        ["Drive Boat To Hydra"] = false,
-        ["Boost Fps"] = false,
-        ["Webhook Drive To Tiki/Hydra"] = false,
-        ["Auto Chest Hop"] = false,
-        ["Webhook Find Leviathan"] = false,
-        ["Auto Craft Scroll"] = false,
-        ["Account Buy Boat"] = true,
-        ["Auto Store Fruit"] = false,
-        ["Start Hunt Leviathan"] = true
-    }
+getgenv().Config = {
+	["Shoot Heart When Ice Spike Breaks"] = true,
+	["Drive Boat To Tiki"] = true,
+	["No Frog"] = true,
+	["Random Devil Fruit"] = false,
+	["Use skill fast dont hold"] = true,
+	["Select Sea Events"] = {
+
+	},
+	["Select Rarity Scroll "] = {
+		[1] = "Mythical"
+	},
+	["Webhook Shoot Heart Leviathan"] = false,
+	["Select Skills Blox Fruit"] = {
+
+	},
+	["Webhook Destroy IDK"] = false,
+	["Auto Farm Material Sanguine Art"] = false,
+	["Webhook Unlock Draco v4"] = false,
+	["Auto light the torch"] = false,
+	["Boost Fps"] = false,
+	["Use Click M1 Fruit"] = false,
+	["Use New Method Shoot Heart"] = false,
+	["Webhook Drive To Tiki/Hydra"] = false,
+	["Webhook Find Leviathan"] = false,
+	["Ping Discord"] = false,
+	["Use New  Method Drive"] = false,
+	["Drive Boat To Hydra"] = false,
+	["Auto Chest Hop"] = false,
+	["Auto Craft Scroll"] = false,
+	["Account Buy Boat"] = true,
+	["Auto Store Fruit"] = false,
+	["Start Hunt Leviathan"] = true
+}
 else
     getgenv().Config = {
 	["Shoot Heart When Ice Spike Breaks"] = true,
@@ -38,7 +47,7 @@ else
 	["No Frog"] = true,
 	["Use New Method Shoot Heart"] = false,
 	["Random Devil Fruit"] = false,
-	["Use skill fast dont hold"] = false,
+	["Use skill fast dont hold"] = true,
 	["Select Sea Events"] = {
 
 	},
@@ -75,4 +84,5 @@ end
 
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Key = "e9162fb60364a89d94d75009"
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-KaitunLevi.lua"))()
